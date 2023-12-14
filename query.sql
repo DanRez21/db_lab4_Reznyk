e43846cff33ab3d1cbb9549edcac19071398695a
DanRez21 <90154810+DanRez21@users.noreply.github.com>
2023-12-14 22:55:36 +0200

    Add files via upload

--- query.sql
+++ query.sql
@@ -1,22 +1,20 @@
--- 1.Вивести вина, які були вирощені в Іспанії
-SELECT title 
+--1. Вивести середню оцінку вин для кожного дегустатора
+SELECT taster_name, AVG(e.points) AS average_evaluation
 FROM wine w
-JOIN location_ l on w.wine_id = l.wine_id
-WHERE l.country = 'Spain';
+JOIN evaluation e ON w.eva_id = e.eva_id
+JOIN taster ta ON ta.taster_id = e.taster_id
+GROUP BY taster_name;
 
--- 2.Вивести ім'я та тег дегустатора, який поставив найвищий бал вину з США, як одне ім'я highest_rated_taster
-SELECT CONCAT(TRIM(taster_name), ', ', TRIM(taster_twitter_handle)) AS highest_rated_taster
-FROM taster
-JOIN evaluation ON taster.taster_id = evaluation.taster_id
-JOIN wine ON wine.eva_id = evaluation.eva_id
-JOIN location_ ON wine.wine_id = location_.wine_id
-WHERE location_.country = 'US'
-ORDER BY evaluation.points DESC
-LIMIT 1;
+--2.Вивести розподіл вин за місцем вирощування
+SELECT country, COUNT(*) AS wine_count
+FROM wine w
+JOIN location_ l ON w.wine_id = l.wine_id
+GROUP BY country;
 
--- 3. Вивести провінцію наступного після найдорожчого вина.
-SELECT province
-FROM location_
-JOIN wine ON wine.wine_id = location_.wine_id
-ORDER BY wine.price DESC
-LIMIT 1 OFFSET 1;
+--3.Вивести залежність між рейтингами вин і різними параметрами
+SELECT AVG(e.points) AS average_evaluation, l.country 
+FROM wine w
+JOIN evaluation e ON w.eva_id = e.eva_id
+JOIN taster ta ON ta.taster_id = e.taster_id
+JOIN location_ l ON l.wine_id = w.wine_id
+GROUP BY l.country;
